5aabc246f2596c61fafc5fab538da6566b1c2675
Enriko Riba <eri@spyro-soft.com>
2023-06-04 15:31:48 +0200

    enhances logging

--- src/OpenRender/Log.cs
+++ src/OpenRender/Log.cs
@@ -2,33 +2,61 @@
 
 public static class Log
 {
-    public static void Debug(string message, params object[] args)
+    public const int LevelDebug = 0;
+    public const int LevelInfo = 1;
+    public const int LevelWarn = 2;
+    public const int LevelError = 3;
+
+    public static int MinimumLevel { get; set; }
+
+    public static void Debug(string message) => WriteLog(LevelDebug, message);
+    public static void Debug(string message, params object[] args) => Debug(string.Format(message, args));
+
+    public static void Info(string message) => WriteLog(LevelInfo, message);
+    public static void Info(string message, params object[] args) => Info(string.Format(message, args));
+
+    public static void Warn(string message) => WriteLog(LevelWarn, message);
+    public static void Warn(string message, params object[] args) => Warn(string.Format(message, args));
+
+    public static void Error(string message) => WriteLog(LevelError, message);
+    public static void Error(string message, params object[] args) => Error(string.Format(message, args));
+
+    private static void WriteLog(int level, string message)
     {
-        WriteColored(ConsoleColor.White, "[DEBUG]");
-        Console.WriteLine(" " + message, args);
+        if (level >= MinimumLevel)
+        {
+            ConsoleColor textColor = ConsoleColor.DarkGray;
+            switch (level)
+            {
+                case LevelDebug:
+                    WriteWithTimeStamp(ConsoleColor.DarkGray, "[DEBUG] ");
+                    break;
+                case LevelInfo:
+                    WriteWithTimeStamp(ConsoleColor.Green, "[INFO] ");
+                    textColor = ConsoleColor.Gray;
+                    break;
+                case LevelWarn:
+                    WriteWithTimeStamp(ConsoleColor.Yellow, "[WARN] ");
+                    textColor = ConsoleColor.DarkYellow;
+                    break;
+                default:
+                    WriteWithTimeStamp(ConsoleColor.Red, "[ERROR] ");
+                    textColor = ConsoleColor.Yellow;
+                    break;
+            }
+            WriteColored(textColor, message);
+            Console.WriteLine();
+        }
     }
 
-    public static void Info(string message, params object[] args)
+    private static void WriteWithTimeStamp(ConsoleColor color, string message)
     {
-        WriteColored(ConsoleColor.Green, "[INFO]");
-        Console.WriteLine(" " + message, args);
-    }
-
-    public static void Warn(string message, params object[] args)
-    {
-        WriteColored(ConsoleColor.Yellow, "[WARN]");
-        Console.WriteLine(" " + message, args);
-    }
-
-    public static void Error(string message, params object[] args)
-    {
-        WriteColored(ConsoleColor.Red, "[ERROR]");
-        Console.WriteLine(" " + message, args);
+        Console.Write($"{DateTime.Now.ToLocalTime()} ");
+        WriteColored(color, message);
     }
 
     private static void WriteColored(ConsoleColor color, string message)
     {
-        Console.Write($"{DateTime.Now.ToLocalTime()} ");
         var currentColor = Console.ForegroundColor;
         Console.ForegroundColor = color;
         Console.Write(message);
